91b75086d31be902abc5c40be831b7edfbe81529
hong-in-yeong <hongreat95@gmail.com>
2021-12-15 16:47:12 +0900

    [#3] AttentionLayer 구조생성

--- cleanformer/models.py
+++ cleanformer/models.py
@@ -15,8 +15,9 @@ class Transformer(LightningModule):
         # 학습을 해야하는 레이어 => 임베딩 테이블, 인코더, 디코더 를 학습한다!
         self.token_embeddings = torch.nn.Embedding(num_embeddings=vocab_size,embedding_dim=hidden_size)
         self.layer = torch.nn.Module()
-        self.encoder = ...
-        self.decoder = ...
+        self.encoder = Encoder(hidden_size)
+        self.decoder = Decoder
+
     def forward(self, src_ids:torch.LongTensor, tgt_ids:torch.Tensor,
                 src_key_padding_mask:torch.Tensor,tgt_key_padding_mask:torch.Tensor) -> torch.Tensor:
         #src_ids 는 임베딩 테이블!
@@ -99,8 +100,57 @@ class Transformer(LightningModule):
 
 class Encoder(torch.nn.Module):
 
-    pass
+    def __init__(self, hidden_size: int):
+        super().__init__()
+
+        self.self_attention_layer = AttentionLayer(hidden_size)
+
+        #TODO - ffn
+
+    def forward(self, x:torch.Tensor):
+
+        contexts = self.self_attention_layer.forward(q=x, k=x, v=x) # 단어가 쓰인 문장에서 단어가 가지는 의미를 임베딩 벡터에 인코딩해준다
 
 class Decoder(torch.nn.Module):
 
     pass
+
+class AttentionLayer(torch.nn.Module):
+    # TODO - multi head attention
+    def __init__(self, hidden_size: int):
+        super().__init__()
+        self.linear_q = torch.nn.Linear(hidden_size,hidden_size)
+        self.linear_k = torch.nn.Linear(hidden_size,hidden_size)
+        self.linear_v = torch.nn.Linear(hidden_size,hidden_size)
+        self.linear_o = torch.nn.Linear(hidden_size,hidden_size)
+
+    def forward(self, q:torch.Tensor, k: torch.Tensor, v: torch.Tensor) -> torch.Tensor:
+
+        """
+        q: (N,L,H)
+        k: (N,L,H)
+        v: (N,L,H)
+        return => (N,L,H)
+
+        """
+        q = self.linear_q(q) #(N, L, H) * (H,H) -> (N,L,H)
+        k = self.linear_k(k) #(N, L, H) * (H,H) -> (N,L,H)
+        v = self.linear_v(v) #(N, L, H) * (H,H) -> (N,L,H)
+
+        # TODO - scaled
+        #유사도 구하기 K(키)를 transpose함. 더 좋은 방법은 einsum을 이용!
+        #h 차원에 대해서 벡터의 내적이 계산되고 h의 차원은 감소한다.
+        sims = torch.einsum("nlh,nlh->nll",q, k)  #(N,L,H) * (N,L,H) -> (N,L,L)
+
+        # TODO - masking(auto-regressive)
+
+        attentions = torch.softmax(sims, dim=2)
+
+        # 가중평균
+
+        contexts = torch.einsum("nij,njh->njh", attentions, v) # (N,L,L) * (N,L,H) -> (N,L,H)
+
+        contexts = self.linear_o(contexts) # (N,L,H) -> (N,L,H)
+        return contexts
+
+
